32c56c7e453391098407230c2161bd513b3887e9
iamasteriix <59852540+iamasteriix@users.noreply.github.com>
2026-06-17 09:39:09 -0700

    feat: added jose jwt

--- apps/server/src/config/env.ts
+++ apps/server/src/config/env.ts
@@ -1,5 +1,6 @@
 const requiredEnvVar = (key: string): string => {
   const value = process.env[key];
+  console.log({ [key]: value });
   if (!value) throw new Error(`Missing required environment variable: ${key}`);
   return value;
 }
@@ -10,18 +11,22 @@ export const env = {
   ENDPOINT: requiredEnvVar('ENDPOINT'),
   NODE_ENV: requiredEnvVar('NODE_ENV'),
 
-  // Postgres
+  // postgres
   PG_HOST: requiredEnvVar('PG_HOST'),
   PG_PORT: parseInt(requiredEnvVar('PG_PORT')),
   PG_DATABASE: requiredEnvVar('PG_DATABASE'),
   PG_USER: requiredEnvVar('PG_USER'),
   PG_PASSWORD: requiredEnvVar('PG_PASSWORD'),
 
-  // Redis
+  // redis
   REDIS_HOST: requiredEnvVar('REDIS_HOST'),
   REDIS_PORT: parseInt(requiredEnvVar('REDIS_PORT')),
   REDIS_PASSWORD: requiredEnvVar('REDIS_PASSWORD'),
 
-  // Logging
+  // logging
   LOG_LEVEL: requiredEnvVar('LOG_LEVEL'),
+
+  // auth
+  JWT_PRIVATE_KEY_B64: requiredEnvVar('JWT_PRIVATE_KEY_B64'),
+  JWT_PUBLIC_KEY_B64: requiredEnvVar('JWT_PUBLIC_KEY_B64'),
 } as const;
--- apps/server/src/middleware/lifecycle/onStart.ts
+++ apps/server/src/middleware/lifecycle/onStart.ts
@@ -3,6 +3,7 @@ import { json, urlencoded, } from "express";
 import { dbPool } from "../../config/db.js";
 import { onRequestLogging } from "../logging/onRequestLogging.js";
 import { logger } from "../../config/logger.js";
+import { initKeys } from "../../config/keys.js";
 import v1Router from "../../routes/v1/index.js";
 
 
@@ -23,6 +24,9 @@ export const onStart = async ({ app }: StartParams): Promise<void> => {
   // connect to db
   const client = await dbPool.connect();
   client.release();
+
+  // initialize JWT keys
+  await initKeys();
 
   // register logging
   app.use(onRequestLogging);
